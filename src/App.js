1d157273c785dd6f9302ba6c3b6e6f1c99e7598d
prabakaran <mspk028@gmail.com>
2023-03-29 10:26:35 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
-import "./styles/App.css";
-import "./styles/normalize.css";
+import Home from "./pages/Home";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// pages
+import Detail from "./pages/Detail";
+import AddEditBlog from "./pages/AddEditBlog";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/detail/:id" element={<Detail />} />
+          <Route path="/create" element={<AddEditBlog />} />
+          <Route path="/update/:id" element={<AddEditBlog />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
