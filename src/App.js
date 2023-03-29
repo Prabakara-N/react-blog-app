38330f0ca32157858317cec8cc2cc2755222e106
prabakaran <mspk028@gmail.com>
2023-03-29 09:50:21 +0530

    packages installed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles/App.css";
+import "./styles/normalize.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default App;
