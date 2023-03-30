b678e3597c5613d9109c5a1e7d07022133ff4f6c
prabakaran <mspk028@gmail.com>
2023-03-30 15:39:57 +0530

    ...

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import App from "./App";
 import "./styles/App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/css/bootstrap.css";
-
-// import "bootstrap-icons/font/bootstrap-icons.css";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
