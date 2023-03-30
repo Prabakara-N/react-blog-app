786ef98104847042edf149b150b5b6657c81472c
prabakaran <mspk028@gmail.com>
2023-03-30 13:22:51 +0530

    like & comments added

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const ScrollToTop = () => {
-  return <div>ScrollToTop</div>;
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 200) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
+  return (
+    <div className="scroll-to-top">
+      {isVisible && (
+        <span onClick={scrollToTop}>
+          <i className="fa fa-arrow-up" />
+        </span>
+      )}
+    </div>
+  );
 };
 
 export default ScrollToTop;
